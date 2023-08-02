16b42e8844b237c909c71f2e0005bc31fd039f86
William Ma <whoiswillma@gmail.com>
2023-08-02 09:17:19 -0700

    Initial Commit

---
+++ LeaTea/LeaTeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaTeaApp.swift
+//  LeaTea
+//
+//  Created by William Ma on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LeaTeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
